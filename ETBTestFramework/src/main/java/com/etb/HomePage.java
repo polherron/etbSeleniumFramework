7057d0d8ae3a3122423285acab6ff734da69d808
polherron <pol.herron@gmail.com>
2019-11-11 14:31:51 +0000

    Updated Title String to correct test

--- ETBTestFramework/src/main/java/com/etb/HomePage.java
+++ ETBTestFramework/src/main/java/com/etb/HomePage.java
@@ -3,14 +3,22 @@ package com.etb;
 public class HomePage {
 	
 	static String url = "https://www.donegaletb.ie/";
-	static String title = "Donegal ETB &#8211; Education and Training Services";
+	static String title = "Donegal ETB – Education and Training Services";
 
 	public void goTo() {
 		Browser.goTo(url);
 		
 	}
+	
+	public static void print(String message) {
+		System.out.println(message);
+	}
 
 	public boolean isAt() {
+		print("Actual");
+		print(Browser.title());
+		print("Expected"); 
+		print (title);
 		return Browser.title().equals(title);
 	}
 
