af0c2f06d86d0bb495aedb4acc7397f31827ac26
polherron <pol.herron@gmail.com>
2019-12-23 11:37:08 +0000

    Added wait to menu constructor
    # WARNING: head commit changed in the meantime
    
    Merge branch 'V3PageObjectModel' into V2PathsPages
    
    Conflicts:
    	ETBTestFramework/src/main/java/com/etb/HomePage.java
    	ETBTestFramework/src/main/java/com/etb/Menu.java
    	ETBTests/.classpath
    	ETBTests/pom.xml
    	ETBTests/src/test/java/com/etb/InitialTests.java

--- ETBTestFramework/src/main/java/com/etb/Menu.java
+++ ETBTestFramework/src/main/java/com/etb/Menu.java
@@ -2,20 +2,40 @@ package com.etb;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class Menu extends Page {
 	
+	WebDriverWait wait; 
+	
+	
+	/**
+	 * 
+	 */
+	public Menu() {
+		super();
+		waitUntilClickable();
+	}
+
+	private void waitUntilClickable() {
+		new WebDriverWait(Browser.driver, 60)
+        .until(ExpectedConditions.elementToBeClickable(musicLink));
+	}
+
 	//Examples of using @FindBy annotation.
 	@FindBy(linkText = "Schools & Youth Services")
 	public WebElement schoolsAndYouthServicesLink;
 	
 	@FindBy(linkText = "Music Education")
 	public WebElement musicLink; 
-	
+		
 	@FindBy(linkText = "Post-Primary Schools")
 	public WebElement postPrimarySchoolsLink; 
+	
 }
 
